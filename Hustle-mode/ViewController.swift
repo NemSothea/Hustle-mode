d6dd05e53290a92ffe96ed04357d79078bb7cdff
NemSothea <nemsothea13@gmail.com>
2017-09-28 15:01:08 +0700

    finish

--- Hustle-mode/ViewController.swift
+++ Hustle-mode/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var powerBtn: UIButton!
+    @IBOutlet weak var cloudHolder: UIView!
+    @IBOutlet weak var rocketStream: UIImageView!
+    @IBOutlet weak var cloudStream: UIImageView!
+    @IBOutlet weak var hustleModelbl: UILabel!
+    @IBOutlet weak var onLbl: UILabel!
+    @IBOutlet weak var darkBlueBg: UIImageView!
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func powerBtnPress(_ sender: Any) {
+        cloudHolder.isHidden = false
+        powerBtn.isHidden = true
+        darkBlueBg.isHidden = true
+        
+        UIView.animate(withDuration: 2.9, animations: {
+           self.rocketStream.frame = CGRect(x: 0, y: 120, width: 375, height: 402)
+            print("Man")
+        }){ (finished) in
+            self.hustleModelbl.isHidden = false
+            self.onLbl.isHidden = false
+            print("here")
+        }
+    }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
